cbd90ae7ee0c5989f4606c90162545b9bea1ae35
Carlos Mourullo <carlosmourullo@gmail.com>
2023-08-02 13:43:53 +0200

    refactor: implemented method to get quotes

--- quote/QuoteDraft.ts
+++ quote/QuoteDraft.ts
@@ -4,8 +4,12 @@ import { BusinessUnit } from '../business-unit/BusinessUnit';
 import { Money } from '../product/Money';
 import { Store } from '../store/Store';
 import { LineItem } from '../cart/LineItem';
+<<<<<<< Updated upstream:saas/project-libraries/b2b/types/quotes/QuoteDraft.ts
 import { QuoteRequestReference } from './QuoteRequest';
 import { StagedQuoteReference } from './StagedQuote';
+=======
+import { Tax } from '../cart/Tax';
+>>>>>>> Stashed changes:saas/project-libraries/b2b/types/quote/QuoteDraft.ts
 
 export enum QuoteDraftState {
   Accepted = 'Accepted', // Accepted by the seller.
@@ -20,11 +24,16 @@ export enum QuoteDraftState {
 export interface QuoteDraft {
   quoteDraftId?: string;
   key?: string;
+<<<<<<< Updated upstream:saas/project-libraries/b2b/types/quotes/QuoteDraft.ts
   version?: number;
   createdAt?: Date;
   lastModifiedAt?: Date;
   // quoteRequest?: QuoteRequestReference;
   // stagedQuote: StagedQuoteReference;
+=======
+  createdAt?: Date;
+  lastModifiedAt?: Date;
+>>>>>>> Stashed changes:saas/project-libraries/b2b/types/quote/QuoteDraft.ts
   account?: Account;
   sellerComment?: string;
   buyerComment?: string;
@@ -32,9 +41,15 @@ export interface QuoteDraft {
   businessUnit?: BusinessUnit;
   lineItems?: LineItem[];
   sum?: Money;
+<<<<<<< Updated upstream:saas/project-libraries/b2b/types/quotes/QuoteDraft.ts
   shippingAddress?: Address;
   billingAddress?: Address;
   // country?: string;
+=======
+  taxed?: Tax;
+  shippingAddress?: Address;
+  billingAddress?: Address;
+>>>>>>> Stashed changes:saas/project-libraries/b2b/types/quote/QuoteDraft.ts
   quoteDraftState?: QuoteDraftState;
   itemShippingAddresses?: Address[];
   expirationDate?: Date;

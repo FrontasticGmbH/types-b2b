966911b7a18b8b314cc856893459a92efed9b8e8
Joel Fickson <Joelngozo@gmail.com>
2023-10-11 12:46:32 +0200

    chore: revert paginated export format

--- result/index.ts
+++ result/index.ts
@@ -2,6 +2,6 @@ import { Facet, FacetTypes } from './Facet';
 import { RangeFacet } from './RangeFacet';
 import { Term } from './Term';
 import { TermFacet } from './TermFacet';
+import { PaginatedResult } from './PaginatedResult';
 
-export { type Facet, type FacetTypes, type RangeFacet, type Term, type TermFacet };
-export * from './PaginatedResult';
+export { type Facet, type FacetTypes, type RangeFacet, type Term, type TermFacet, type PaginatedResult };
